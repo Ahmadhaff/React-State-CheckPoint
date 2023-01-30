3c1e6db4937488c4eaa28a16f35660ee80ae8a14
haffouadhi ahmad <haffoudhiahmed219@mail.com>
2023-01-30 19:19:25 +0100

    React-State-CheckPoint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import "./App.css";
 import Checkpoint from "./Checkpoint";
 
-function App() {
-  return (
-    <div className="App">
-      <Checkpoint />
-    </div>
-  );
-}
+import React, { Component } from 'react'
 
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Person: {
+        fullName: "Haffoudhi Ahmad",
+        bio: "FullStack Developper",
+        imgSrc:
+          "https://www.namasteui.com/wp-content/uploads/2019/07/Full-Stack-Developer.jpg",
+        profession: "Student",
+      },
+      shows: true,
+      mountingTime: 0,
+    };
+  }
+  change = () => {
+    this.setState({ shows: !this.state.shows });
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.shows ? <Checkpoint></Checkpoint> :
+        null}
+       <button onClick={this.change}>Show</button> 
+      </div>
+    )
+  }
+}
--- src/Checkpoint.js
+++ src/Checkpoint.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+
+
 export default class Checkpoint extends Component {
   constructor(props) {
     super(props);
@@ -15,30 +17,26 @@ export default class Checkpoint extends Component {
       mountingTime: 0,
     };
   }
-  change = () => {
-    this.setState({ shows: !this.state.shows });
-  };
 
   componentDidMount() {
     setInterval(() => {
       // eslint-disable-next-line react/no-direct-mutation-state
-      this.setState({ mountingTime: (this.state.mountingTime += 0.5) });
+      this.setState({ mountingTime: this.state.mountingTime + 1 });
     }, 1000);
   }
   render() {
     return (
       <div>
-        {this.state.shows ? (
+         
           <>
-            {" "}
             <h1>My Name Is {this.state.Person.fullName}</h1>
             <h1>Am a {this.state.Person.bio}</h1>
             <h1>But am Still {this.state.Person.profession} In GoMyCode</h1>
             <img src={this.state.Person.imgSrc} alt="Developper"></img>
             <h1> mounting-Time = {this.state.mountingTime}</h1>
           </>
-        ) : null}
-        <button onClick={this.change}>show</button>
+        
+        
       </div>
     );
   }
